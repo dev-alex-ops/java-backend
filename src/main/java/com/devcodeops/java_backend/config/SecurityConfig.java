40df59ea53a05e7ed343d41e8116d91cdb5d730f
Alejandro Salas Barceló <devalexops@gmail.com>
2025-07-25 00:47:52 +0200

    Initial commit: Added list all users and items and delete item methods. Added security config to encode passwords and connections. Avoid securitized endpoints by now

---
+++ src/main/java/com/devcodeops/java_backend/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.devcodeops.java_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable) // Esta es la forma aún vigente (no eliminada, solo advertida)
+                .build();
+    }
+}
